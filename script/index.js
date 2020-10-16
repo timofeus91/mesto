5d82a79a2deaafcf4c0355ad591cc94ef9462e2d
timofeus91 <berezhnov.timofei@yandex.ru>
2020-10-16 19:13:06 +0300

    Застрял на формах2

--- script/index.js
+++ script/index.js
@@ -1,7 +1,16 @@
-//тут будет код js. Надеюсь что будет.)
+//переменные
 
 let OpenPopup = document.querySelector('.profile__edit-button');
+let ClosePopup = document.querySelector('.popup__close');
+let SavaChangesPopup = document.querySelector('.popup__save');
+let NamePopup = document.querySelector('.popup__name');
+let ProfessionPopup = document.querySelector('.popup__profession');
 
+
+
+
+
+//проверка по окончании скрипта на его работу (ну или что-то похожее)
 function showClick() {
     alert('ты подключил js красавааа!');
 }
