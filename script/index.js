8bdc0903a6738c99bc5ca462f796d8dfb15fb352
timofeus91 <berezhnov.timofei@yandex.ru>
2020-10-16 15:50:50 +0300

    начал подключать js

--- script/index.js
+++ script/index.js
@@ -1,2 +1,10 @@
 //тут будет код js. Надеюсь что будет.)
 
+let OpenPopup = document.querySelector('.profile__edit-button');
+
+function showClick() {
+    alert('ты подключил js красавааа!');
+}
+
+OpenPopup.addEventListener('click', showClick);
+
