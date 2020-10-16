904e159949be73de91ce442a8607a27fc9d2ba25
timofeus91 <berezhnov.timofei@yandex.ru>
2020-10-17 00:04:16 +0300

    Добавил открытие-закрытие попапа

--- script/index.js
+++ script/index.js
@@ -1,19 +1,22 @@
-//переменные
+console.log('Hello world');
+
+// все нужные переменные
 
 let OpenPopup = document.querySelector('.profile__edit-button');
 let ClosePopup = document.querySelector('.popup__close');
 let SavaChangesPopup = document.querySelector('.popup__save');
 let NamePopup = document.querySelector('.popup__name');
 let ProfessionPopup = document.querySelector('.popup__profession');
+let popup = document.querySelector('.popup');
+let NameFromDoc = document.querySelector('.profile__title');
+let ProfessionFromDoc = document.querySelector('.profile__profile__subtitle');
 
 
-
-
-
-//проверка по окончании скрипта на его работу (ну или что-то похожее)
-function showClick() {
-    alert('ты подключил js красавааа!');
+const PopupToggle = () => {
+    popup.classList.toggle('popup_opened')
 }
 
-OpenPopup.addEventListener('click', showClick);
+OpenPopup.addEventListener("click", PopupToggle)
+ClosePopup.addEventListener("click", PopupToggle)
+
 
