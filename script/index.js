7f99a06f98ff53eb4c02ac0b1d29d7399de1799a
timofeus91 <berezhnov.timofei@yandex.ru>
2020-12-28 12:36:34 +0300

    Problems with validate and inputs atributs

--- script/index.js
+++ script/index.js
@@ -182,14 +182,19 @@ closeImgPopup.addEventListener('click', function () {
     closePopup(popupImg);
 });
 
-enableValidation({
+
+
+
+//объект со списком ключей-значений для запуска функций связанных с валидацией
+const validationConfig = {
     formSelector: '.popup__form',
     inputSelector: '.popup__input',
     submitButtonSelector: '.popup__button',
     inactiveButtonClass: 'popup__button_disabled',
     inputErrorClass: 'popup__input_error',
     errorClass: 'popup__error_visible'
-  }); 
+  }
 
-  
-  
+
+//запуск функции валидации форм 
+enableValidation(validationConfig); 
--- script/validate.js
+++ script/validate.js
@@ -1,66 +1,66 @@
 
 // функция которая показывает ошибку
-function showError(form, input) {
+function showError(form, input, config) {
   const error = form.querySelector(`#${input.id}-error`);
   error.textContent = input.validationMessage;
-  input.classlist.add(inputErrorClass);
+  input.classlist.add(config.inputErrorClass);
+  
 }
 
 // функция которая скрывает ошибку
 
-function hideError(form, input) {
+function hideError(form, input, config) {
   const error = form.querySelector(`#${input.id}-error`);
   error.textContent = '';
-  input.classlist.remove(inputErrorClass);
+  input.classlist.remove(config.inputErrorClass);
+  
 }
 
 //функция которая проверяет конкретный input на валидность
 
-function checkInputValidity(form, input) {
+function checkInputValidity(form, input, config) {
   if (input.validity.valid) {
-    hideError(form, input);
+    hideError(form, input, config);
   } else {
-    showError(form, input);
+    showError(form, input, config);
   }
 }
 
 // функция по проверке кнопки отправки формы и включению-выключению ее активности
-function setButtonState(button, isActive) {
+function setButtonState(button, isActive, config) {
   if (isActive) {
-    button.classlist.remove(inactiveButtonClass)
+    button.classlist.remove(config.inactiveButtonClass)
     button.disabled = false;
   } else {
-    button.classlist.add(inactiveButtonClass)
+    button.classlist.add(config.inactiveButtonClass)
     button.disabled = true;
   }
 }
 
 //функция по поиску всех input-s и button-s , переборке формы.
-function setEventListener(form){
-  const inputList = document.querySelectorAll(inputSelector);
-  const submitButton = document.querySelector(submitButtonSelector);
+function setEventListener(form, config){
+  const inputList = form.querySelectorAll(config.inputSelector);
+  const submitButton = form.querySelector(config.submitButtonSelector);
 
   inputList.forEach(input => {
   input.addEventListener('input', (evt) => {
-    checkInputValidity(form, input)
-    setButtonState(submitButton, form.checkInputValidity())
+    checkInputValidity(form, input, config)
+    setButtonState(submitButton, form.checkValidity(), config)
   })
 })
 }
 
 // функция по поиску всех форм. Запрет на действия по умолчанию
-function enableValidation(){
-  const forms = document.querySelectorAll(formSelector);
+function enableValidation(config){
+  const forms = document.querySelectorAll(config.formSelector);
   forms.forEach(form => {
-    setEventListener(form)
+    setEventListener(form, config)
 
     form.addEventListener('submit', (evt) => {
       evt.preventDefault();
     })
 
-    const submitButton = document.querySelector(submitButtonSelector);
-    setButtonState(submitButton, form.checkInputValidity())
+    const submitButton = form.querySelector(config.submitButtonSelector);
+    setButtonState(submitButton, form.checkValidity(), config)
   })
 }
-
-
